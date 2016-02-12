64b5ee624ee04891832885f6c6009051687bbe89
assafbt <assafbt@gmail.com>
2016-02-12 12:59:57 +0200

    Initial commit

---
+++ app/src/main/java/com/assafbt/projects/memorygame/MainActivity.java
@@ -0,0 +1,21 @@
+/*
+ * *
+ *  * Created by Assaf Biton
+ *
+ */
+
+package com.assafbt.projects.memorygame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            //init1
+    }
+}
