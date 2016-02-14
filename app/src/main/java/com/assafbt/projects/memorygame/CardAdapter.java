3a4865e9d07f30672fcf1cbff29ade9ce911cc76
assafbt <assafbt@gmail.com>
2016-02-14 18:38:47 +0200

    add object Card, set portrait screen only

---
+++ app/src/main/java/com/assafbt/projects/memorygame/CardAdapter.java
@@ -0,0 +1,18 @@
+/*
+ * *
+ *  * Created by Assaf Biton
+ *
+ */
+
+package com.assafbt.projects.memorygame;
+
+/**
+ * Created by assafbt on 14/02/2016.
+ */
+public class CardAdapter {
+
+
+
+
+
+}
