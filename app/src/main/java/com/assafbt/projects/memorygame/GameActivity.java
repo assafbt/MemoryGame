0b9284c169965aaeb2fef6d29968717ccb107a43
anttross <anttross@gmail.com>
2016-02-14 21:32:48 +0200

    Game Activity Picasso fixed

--- app/src/main/java/com/assafbt/projects/memorygame/GameActivity.java
+++ app/src/main/java/com/assafbt/projects/memorygame/GameActivity.java
@@ -1,13 +1,35 @@
 package com.assafbt.projects.memorygame;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class GameActivity extends AppCompatActivity {
+
+    Card card00;
+    ImageView im00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_game);
+
+        card00 = new Card();
+        card00.setImgUp("http://openweathermap.org/img/w/10d.png");
+
+        //System.out.println("###" + card00.getImgUp());
+
+        im00 = (ImageView)findViewById(R.id.img03);
+
+            Picasso.with(getApplicationContext()).load(card00.getImgUp())
+                    .placeholder(R.drawable.ic_launcher) // optional
+                    .into(im00);
+
+
+
+
     }
 }
--- app/src/main/java/com/assafbt/projects/memorygame/MainActivity.java
+++ app/src/main/java/com/assafbt/projects/memorygame/MainActivity.java
@@ -6,25 +6,29 @@
 
 package com.assafbt.projects.memorygame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            /*
-            *
-            * make new project
-            * press "ignore" (not "add root")
-            * goto VCS-> import into version control->share on github
-            * and only than the project will get it's first init
-            * good luck 
-            *
-             */
+           start = (Button)findViewById(R.id.startBTN);
+             start.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), GameActivity.class));
+
+            }
+        });
 
 
     }
