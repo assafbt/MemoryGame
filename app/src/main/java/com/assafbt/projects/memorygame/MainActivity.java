5a861c8959824253b5bbf22e61c7c8019a0ecd42
assaf <assafbt@gmail.com>
2019-10-31 01:01:18 +0200

    MainActivity: new design

--- app/src/main/java/com/assafbt/projects/memorygame/MainActivity.java
+++ app/src/main/java/com/assafbt/projects/memorygame/MainActivity.java
@@ -10,9 +10,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -21,15 +24,20 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class MainActivity extends Activity {
-    Button start, select, reset1,reset2, aboutBtn;
+    Button select, reset1,reset2, aboutBtn,start, languageBtn;
     private static int RESULT_LOAD_IMG = 8;
     String imgDecodableString;
     int step = 0;
+
     SharedPreferences prefs;
     SharedPreferences.Editor editor;
     DAL dalObj = new DAL(this);
     SQLiteDatabase db;
+    Locale locale;
+    boolean isEnglishLanguage;
     BestTime_DBHelper bestTimeDbHelper;
     String imagUri[][]= new String[8][2];
     int drawable_images[]= new int[]{
@@ -51,6 +59,8 @@ public class MainActivity extends Activity {
 
         prefs = getSharedPreferences("shPref", Context.MODE_PRIVATE);
         editor = prefs.edit();
+
+
 
 
         TextView best1v = (TextView) findViewById(R.id.best1View);
@@ -88,7 +98,7 @@ public class MainActivity extends Activity {
                 });
 
 
-           start = (Button)findViewById(R.id.startBTN);
+           start = (Button) findViewById(R.id.startBTN);
              start.setOnClickListener(new View.OnClickListener() {
 
                  @Override
@@ -154,6 +164,41 @@ public class MainActivity extends Activity {
 
         });
 
+        languageBtn = (Button) findViewById(R.id.language_btn);
+        /* setLanguage("he");
+       String language = Locale.getDefault().getLanguage();
+        Log.i("locale", "locale,  " + language);
+        if (language == "en") {
+            isEnglishLanguage = true;
+        }
+        else {
+            isEnglishLanguage = false;
+            language = "he";
+        }
+        languageBtn.setText(language);
+        languageBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("locale","locale, isEnglishLanguage = " + isEnglishLanguage);
+                if (isEnglishLanguage) {
+                    setLanguage("he");
+                    isEnglishLanguage = false;
+                    languageBtn.clearComposingText();
+                    languageBtn.setText("heb");
+                    Log.i("locale","locale, isEnglishLanguage changed to " + isEnglishLanguage);
+
+                }
+                else {
+                    setLanguage("en");
+                    isEnglishLanguage = true;
+                    languageBtn.clearComposingText();
+                    languageBtn.setText("en");
+                    Log.i("locale","locale, isEnglishLanguage changed to " + isEnglishLanguage);
+                }
+            }//onClick
+
+
+        });*/
     }//onCreate
 
     @Override
@@ -218,11 +263,21 @@ public class MainActivity extends Activity {
         } finally {
 
             if (cursor != null) {
-
                 cursor.close();
             }
         }
     }//getRealPathFromURI
 
+    public void setLanguage(String lang)
+    {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
 
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+        this.setContentView(R.layout.activity_main);
+        Log.i("locale","locale, language changed to " + lang);
+    }
 }//MainActivity
